0a5784445fb4bd16d2db50f43ffc62e59d90bb3f
Vitória Maria <109771542+VitoriaMariadev@users.noreply.github.com>
2024-10-04 10:58:22 -0300

    FEAT/ Casting

--- Java/primeiro_projeto/src/Aula002/aula002.java
+++ Java/primeiro_projeto/src/Aula002/aula002.java
@@ -1,5 +1,41 @@
 package Aula002;
 
 public class aula002 {
+	
+	public static void main(String[] args) {
+		
+		//double b, B, h, area;
+		
+		//b = 6.0;
+		//B = 8.0;
+		//h = 5.0;
+		
+		//area = (b + B) / 2.0 * h;
+		
+		//System.out.println(area);
 
+		//int a, b;
+		//double resultado;
+		
+		//a = 5;
+		//b = 2;
+		
+		//resultado =  5 / 2;
+		
+		//System.out.println(resultado);
+		
+		///resultado = (double) 5 / 2;
+		
+		//System.out.println(resultado);
+		
+		double a;
+		int b;
+		
+		a = 5.0;
+		b = (int) a;
+		
+		System.out.println(b);
+		
+	}
+	
 }
